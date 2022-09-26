4df971d55e2b2278b9b49da5c4da019eeca842d6
DannyMX29 <bran.dan.her.gar@gmail.com>
2022-09-26 13:42:55 -0500

    identacion de codigo y checado de scripts

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,58 +1,64 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');//asignamos a una constante la dependecia para usarla en module -> rules
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');//agregamos la dependecia a una constante para que podamos hacer uso de ella
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-
-module.exports = {
-    entry: './src/index.js',//colocamos el punto de entrada del proyecto
-    output: {//en la salida vamos a colocar path para indicar la ruta y el nombre de la carpeta donde van a quedar los archivos que genere gracias al webpack
-        path: path.resolve(__dirname, 'dist'),//ruta donde vamos a colocar la compilacion mas el nombre de la carpeta resultante
-        filename: 'bundle.js'//nombre del archivo que va a resultar de la compilacion
-    },
-    resolve:{//en el resolve vamos a colocar las extenciones con las que vamos a trabajar
-        extensions: ['.js', '.jsx']
-    },
+module.exports = 
+    {
+    entry: './src/index.js',
+    output: {
+                path: path.resolve(__dirname, 'dist'),
+                filename: 'bundle.js'
+            },
+    resolve:{
+                extensions: ['.js', '.jsx']
+            },
     mode: 'development',
-    module: {//dentro del modulo vamos a colocar las reglas que nos van ayudar a trabajar con las dependencias
-        rules: [
-            {
-                test: /\.(js|jsx)$/,//en esta parte estamos haciendo el llamado a las extenciones js o jsx
-                exclude: /node_modules/,//excluimos la carpeta node_modules para exitar problemas con su contenido
-                use: {//pasamos el loader
-                    loader: 'babel-loader',//la dependencia loader que instalamos
+    module: {
+                rules:  [
+                            {
+                                test: /\.(js|jsx)$/,
+                                exclude: /node_modules/,
+                                use:{
+                                        loader: 'babel-loader',
+                                    }
+                            },
+                            {
+                                test: /\.html$/,
+                                use:[
+                                        {
+                                            loader: 'html-loader'
+                                        }
+                                    ]
+                            },
+                            {
+                                test: /\.s[ac]ss$/,
+                                use:[
+                                        'style-loader',
+                                        'css-loader',
+                                        'sass-loader'
+                                    ]                
+                            },
+                        ]
+            },
+    plugins:[
+                new HtmlWebpackPlugin   (
+                                            {
+                                                template: './public/index.html',
+                                                filename: './index.html',
+                                            }
+                                        ),
+                new MiniCssExtractPlugin(
+                                            {
+                                                filename: '[name].css'
+                                            }
+                                        )
+            ],
+    devServer:  {
+                    static: {
+                                directory: path.join(__dirname, "dist"),
+                            },
+                    compress: true,
+                    port: 3006,
+                    open: true
                 }
-            },
-            {
-                test: /\.html$/,//en esta parte estamos haciendo el llamado a las extenciones html
-                use: [{
-                    loader: 'html-loader'
-                }]
-            },
-            {
-                test: /\.s[ac]ss$/,//creamos la exprecion regular para llamar los archivos de css o sass
-                use: [//asignamos cuales loader son loader
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]                
-            },
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',//le indicamos el origen del archivo
-            filename: './index.html',//le indicamos el nombre del archivo resultante
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css'
-        })
-    ],
-    devServer: {//prepaamos el servidor interno 
-        static: {
-            directory: path.join(__dirname, "dist"),
-          },//el llamado al contenido
-        compress: true,//para comprimirlo
-        port: 3006,//puerto
-        open: true//para que se habra al ejecutar
-    }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,70 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');//asignamos a una constante la dependecia para usarla en module -> rules
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');//agregamos la dependecia a una constante para que podamos hacer uso de ella
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin= require('terser-webpack-plugin');
-const { cleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-    entry: './src/index.js',//colocamos el punto de entrada del proyecto
-    output: {//en la salida vamos a colocar path para indicar la ruta y el nombre de la carpeta donde van a quedar los archivos que genere gracias al webpack
-        path: path.resolve(__dirname, 'dist'),//ruta donde vamos a colocar la compilacion mas el nombre de la carpeta resultante
-        filename: 'bundle.js',//nombre del archivo que va a resultar de la compilacion
-        publicPath: "/",//esta linea le indica el lugar donde lo va crea wl archivo a publicar
-    },
-    resolve:{//en el resolve vamos a colocar las extenciones con las que vamos a trabajar
-        extensions: ['.js', '.jsx'],
-        alias: {//colocamos alias para hacer mas faciles las referencias hacia las carpetas
-            '@components': path.resolve(__dirname, 'src/components/'),
-            '@styles': path.resolve(__dirname, 'src/styles/')
-        }
-    },
-    mode: 'production',
-    module: {//dentro del modulo vamos a colocar las reglas que nos van ayudar a trabajar con las dependencias
-        rules: [
-            {
-                test: /\.(js|jsx)$/,//en esta parte estamos haciendo el llamado a las extenciones js o jsx
-                exclude: /node_modules/,//excluimos la carpeta node_modules para exitar problemas con su contenido
-                use: {//pasamos el loader
-                    loader: 'babel-loader',//la dependencia loader que instalamos
-                }
-            },
-            {
-                test: /\.html$/,//en esta parte estamos haciendo el llamado a las extenciones html
-                use: [{
-                    loader: 'html-loader'
-                }]
-            },
-            {
-                test: /\.s[ac]ss$/,//creamos la exprecion regular para llamar los archivos de css o sass
-                use: [//asignamos cuales loader son loader
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]                
-            },
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',//le indicamos el origen del archivo
-            filename: './index.html',//le indicamos el nombre del archivo resultante
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css'
-        }),
-        new cleanWebpackPlugin(),
-    ],
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin(),
-        ]
+module.exports = 
+    {
+        entry: './src/index.js',
+        output: {
+                    path: path.resolve(__dirname, 'dist'),
+                    filename: 'bundle.js',
+                    publicPath: "/",
+                },
+        resolve:{
+                    extensions: ['.js', '.jsx'],
+                    alias:  {
+                                '@components': path.resolve(__dirname, 'src/components/'),
+                                '@styles': path.resolve(__dirname, 'src/styles/')
+                            }
+                },
+        mode: 'production',
+        module: {
+                    rules: [
+                                {
+                                    test: /\.(js|jsx)$/,
+                                    exclude: /node_modules/,
+                                    use:{
+                                            loader: 'babel-loader',
+                                        }
+                                },
+                                {
+                                    test: /\.html$/,
+                                    use:[
+                                            {   loader: 'html-loader'   }
+                                        ]
+                                },
+                                {
+                                    test: /\.s[ac]ss$/,
+                                    use:[
+                                            'style-loader',
+                                            'css-loader',
+                                            'sass-loader'
+                                        ]                
+                                },
+                            ]
+                },
+        plugins:[
+                    new HtmlWebpackPlugin   (
+                                                {
+                                                    template: './public/index.html',
+                                                    filename: './index.html',
+                                                }
+                                            ),
+                    new MiniCssExtractPlugin(
+                                                {
+                                                    filename: '[name].css'
+                                                }
+                                            ),
+                    new CleanWebpackPlugin(),
+                ],
+        optimization:   {
+                        minimize: true,
+                        minimizer:  [
+                                        new CssMinimizerPlugin(),
+                                        new TerserPlugin(),
+                                    ]
+                        }
     }
-}
